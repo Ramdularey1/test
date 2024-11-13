2f0ef6c3bf8dfe126761c6227c81a8f9f9057912
ram-revspire <ram.d.yadav@revspire.net>
2024-11-13 13:20:18 +0530

    create a button to navigae to Content

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function Home() {
+  const navigate = useNavigate()
+  const handleClick = () => {
+    navigate("/content")
+  }
   return (
-    <div>Home</div>
+    <div>
+      <div>Home</div>
+      <button onClick={handleClick}>Go to Content</button>
+    </div>
   )
 }
 
