5c5c23ffab82cfc65253a2a5edaca357df363ba4
Ramdularey1 <ramdulareyyadav2@gmail.com>
2024-11-16 14:57:38 +0530

    update

--- src/hooks/useOpenGraph/index.js
+++ src/hooks/useOpenGraph/index.js
@@ -1,3 +1,56 @@
+// import { useEffect } from "react"
+
+// const usePage = ({
+//     title,
+//     description,
+//     keywords = [],
+//     ogTitle,
+//     ogDescription,
+//     ogImage,
+//     ogUrl
+//  }) => {
+
+//     useEffect(() => {
+//       document.title = title
+
+//       setMetaTag('name', 'description', description)
+//       setMetaTag('name', 'keywords', keywords)
+//       setMetaTag('property', 'og:title', ogTitle || title)
+//       setMetaTag('property', 'og:description', ogDescription || description)
+//       setMetaTag('property', 'og:image', ogImage)
+//       setMetaTag('property', 'og:url', ogUrl || window.location.href)
+      
+    
+
+//     },[title,
+//         description,
+//         keywords,
+//         ogTitle,
+//         ogDescription,
+//         ogImage,
+//         ogUrl ])
+
+//         const setMetaTag = (attr, key, content) => {
+//             if(content){
+//                 let element = document.querySelector(`meta[${attr}="${key}"]`)
+//                 if(!element){
+//                     element = document.createElement('meta')
+//                     element.setAttribute(attr, key)
+//                     document.head.appendChild(element);
+//                 }
+//                 element.setAttribute('content', content)
+//             }
+
+//         }
+
+// }
+
+// export default usePage
+
+
+
+
+
 import { useEffect } from "react"
 
 const usePage = ({
@@ -14,36 +67,33 @@ const usePage = ({
       document.title = title
 
       setMetaTag('name', 'description', description)
-      setMetaTag('name', 'keywords', keywords)
+      setMetaTag('name', 'keywords', keywords.join(', ')) // Join keywords array into a string
       setMetaTag('property', 'og:title', ogTitle || title)
       setMetaTag('property', 'og:description', ogDescription || description)
       setMetaTag('property', 'og:image', ogImage)
       setMetaTag('property', 'og:url', ogUrl || window.location.href)
       
-    //   return () => {
+    }, [
+      title,
+      description,
+      keywords,
+      ogTitle,
+      ogDescription,
+      ogImage,
+      ogUrl
+    ])
 
-    //   }
-
-    },[title,
-        description,
-        keywords,
-        ogTitle,
-        ogDescription,
-        ogImage,
-        ogUrl ])
-
-        const setMetaTag = (attr, key, content) => {
-            if(content){
-                let element = document.querySelector(`meta[${attr}="${key}"]`)
-                if(!element){
-                    element = document.createElement('meta')
-                    element.setAttribute(attr, key)
-                    document.head.appendChild(element);
-                }
-                element.setAttribute('content', content)
+    const setMetaTag = (attr, key, content) => {
+        if(content){
+            let element = document.querySelector(`meta[${attr}="${key}"]`)
+            if(!element){
+                element = document.createElement('meta')
+                element.setAttribute(attr, key)
+                document.head.appendChild(element)
             }
-
+            element.setAttribute('content', content)
         }
+    }
 
 }
 
